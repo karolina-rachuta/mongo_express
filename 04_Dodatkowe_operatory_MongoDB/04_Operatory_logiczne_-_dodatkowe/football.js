d337df810a7739e0d3c22f62f8aaf0526eb77f59
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-03-11 19:26:58 +0100

    selectors nor

--- 04_Dodatkowe_operatory_MongoDB/04_Operatory_logiczne_-_dodatkowe/football.js
+++ 04_Dodatkowe_operatory_MongoDB/04_Operatory_logiczne_-_dodatkowe/football.js
@@ -10,35 +10,45 @@ const dbName = 'exercises';
 const collectionName = 'footballers';
 
 (async function () {
-  try {
-    // Connect using MongoClient
-    const client = await MongoClient.connect(url, {useUnifiedTopology: true});
-    console.log('Successfully connected to local MongoDB instance.');
+    try {
+        // Connect using MongoClient
+        const client = await MongoClient.connect(url, {useUnifiedTopology: true});
+        console.log('Successfully connected to local MongoDB instance.');
 
-    // Get DB instance
-    const db = client.db(dbName);
+        // Get DB instance
+        const db = client.db(dbName);
 
-    const collection = db.collection(collectionName);
+        const collection = db.collection(collectionName);
 
-    // HERE - modify this "find" code!
-    const footballers = collection.find({});
+        // HERE - modify this "find" code!
+        const footballers = collection.find({
+            $nor: [
+                {"games": {$lte: 4}},
+                {"inactive": true},
+                {"redCards": {$lt: 2}}]
+        });
 
-    // HERE - add your average calculating code here:
-    let avg = 0;
+        // HERE - add your average calculating code here:
+        let avg = 0;
 
-    // Assertions below
-    const footballersArr = await footballers.toArray();
-    console.assert(footballersArr && footballersArr[0] && footballersArr[0].name === 'Port Varley',
-      'Should find Port as the first non-performant footballer', footballersArr);
-    console.assert(Math.round(avg * 100) / 100 === 2.67, 'Should have average goals of 2.67',
-      Math.round(avg * 100) / 100);
+        await footballers.forEach(footballer => {
+            avg += footballer.goals;
+        });
 
-    await client.close();
+        avg = avg / await footballers.count();
+        // Assertions below
+        const footballersArr = await footballers.toArray();
+        console.assert(footballersArr && footballersArr[0] && footballersArr[0].name === 'Port Varley',
+            'Should find Port as the first non-performant footballer', footballersArr);
+        console.assert(Math.round(avg * 100) / 100 === 2.67, 'Should have average goals of 2.67',
+            Math.round(avg * 100) / 100);
 
-    return process.exit(0);
-  } catch (err) {
-    console.log('Something went wrong!', err);
-    return process.exit(1);
-  }
+        await client.close();
+
+        return process.exit(0);
+    } catch (err) {
+        console.log('Something went wrong!', err);
+        return process.exit(1);
+    }
 })();
 
