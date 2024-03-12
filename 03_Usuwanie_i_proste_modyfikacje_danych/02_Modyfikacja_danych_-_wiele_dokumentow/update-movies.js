8439ad0a8e1c35a6b16ec724cad1c1793522fa8f
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-03-12 11:53:57 +0100

    updateMany mongoDB

--- 03_Usuwanie_i_proste_modyfikacje_danych/02_Modyfikacja_danych_-_wiele_dokumentow/update-movies.js
+++ 03_Usuwanie_i_proste_modyfikacje_danych/02_Modyfikacja_danych_-_wiele_dokumentow/update-movies.js
@@ -11,39 +11,67 @@ const dbName = 'exercises';
 const collectionName = 'movies';
 
 (async function () {
-  try {
-    // Connect using MongoClient
-    const client = await MongoClient.connect(url, {useUnifiedTopology: true});
-    console.log('Successfully connected to local MongoDB instance.');
+    try {
+        // Connect using MongoClient
+        const client = await MongoClient.connect(url, {useUnifiedTopology: true});
+        console.log('Successfully connected to local MongoDB instance.');
 
-    // Get DB instance
-    const db = client.db(dbName);
+        // Get DB instance
+        const db = client.db(dbName);
 
-    const collection = db.collection(collectionName);
+        const collection = db.collection(collectionName);
 
-    // HERE - add your update code
+        // HERE - add your update code
+        // collection.updateMany(
+        //     {
+        //         series: {
+        //             $in: ['Mad Max', 'Mission: Impossible']
+        //         }
+        //     },
+        //     {
+        //         $set: {
+        //             seen: true
+        //         }
+        //     })
 
-    // Assertions below - do not modify them!
-    const madMaxMovies = await collection.find({series: 'Mad Max'}).toArray();
-    madMaxMovies.forEach(movie => {
-      console.assert(movie.seen === true, 'Movie should be seen: ', movie.title);
-    });
+        collection.updateMany(
+            {
+                series: 'Mad Max'
+            },
+            {
+                $set: {seen: true}
+            });
 
-    const miMovies = await collection.find({series: 'Mission: Impossible'}).toArray();
-    miMovies.forEach(movie => {
-      console.assert(movie.seen === true, 'Movie should be seen: ', movie.title);
-    });
+        collection.updateMany(
+            {
+                series: 'Mission: Impossible'
+            },
+            {
+                $set: {seen: true}
+            })
 
-    // Check if other movies were not updated
-    const lotrMovie = await collection.findOne({ title: 'The Lord of the Rings: The Two Towers' });
-    console.assert(lotrMovie.seen === false, 'Lord of the Rings should still be unseen!', lotrMovie);
 
-    await client.close();
+        // Assertions below - do not modify them!
+        const madMaxMovies = await collection.find({series: 'Mad Max'}).toArray();
+        madMaxMovies.forEach(movie => {
+            console.assert(movie.seen === true, 'Movie should be seen: ', movie.title);
+        });
+        console.log(madMaxMovies)
+        const miMovies = await collection.find({series: 'Mission: Impossible'}).toArray();
+        miMovies.forEach(movie => {
+            console.assert(movie.seen === true, 'Movie should be seen: ', movie.title);
+        });
+        console.log(miMovies)
+        // Check if other movies were not updated
+        const lotrMovie = await collection.findOne({title: 'The Lord of the Rings: The Two Towers'});
+        console.assert(lotrMovie.seen === false, 'Lord of the Rings should still be unseen!', lotrMovie);
+        console.log(lotrMovie)
+        await client.close();
 
-    return process.exit(0);
-  } catch (err) {
-    console.log('Something went wrong!', err);
-    return process.exit(1);
-  }
+        return process.exit(0);
+    } catch (err) {
+        console.log('Something went wrong!', err);
+        return process.exit(1);
+    }
 })();
 
