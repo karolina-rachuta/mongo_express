fc0bab5f570af04c4fa50990749af13c6030b33c
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-03-12 11:11:20 +0100

    updateOne mongoDB

--- 03_Usuwanie_i_proste_modyfikacje_danych/01_Modyfikacja_danych_-_pojedynczy_dokument/update-number.js
+++ 03_Usuwanie_i_proste_modyfikacje_danych/01_Modyfikacja_danych_-_pojedynczy_dokument/update-number.js
@@ -11,32 +11,45 @@ const dbName = 'exercises';
 const collectionName = 'numbers';
 
 (async function () {
-  try {
-    // Connect using MongoClient
-    const client = await MongoClient.connect(url, {useUnifiedTopology: true});
-    console.log('Successfully connected to local MongoDB instance.');
+    try {
+        // Connect using MongoClient
+        const client = await MongoClient.connect(url, {useUnifiedTopology: true});
+        console.log('Successfully connected to local MongoDB instance.');
 
-    // Get DB instance
-    const db = client.db(dbName);
+        // Get DB instance
+        const db = client.db(dbName);
 
-    const collection = db.collection(collectionName);
+        const collection = db.collection(collectionName);
 
-    // HERE - add your update code
+        // HERE - add your update code
 
-    // Assertions below - do not modify them!
-    const changedContact = await collection.findOne({phoneNo: '48616161616'});
-    console.assert(changedContact !== null, 'Contact should be found by new phone number!', changedContact);
+        collection.updateOne(
+            {
+                $and: [
+                    {firstName: "Jan"},
+                    {lastName: "Kowalski"}
+                ]
+            },
+            {
+                $set: {phoneNo: '48616161616'}
+            })
 
-    const unchangedContact = await collection.findOne({phoneNo: '48754456654'});
-    console.assert(unchangedContact && unchangedContact.firstName === 'Jan' && unchangedContact.lastName === 'Nowacki',
-      'Should find unchanged contact with the same first name', unchangedContact);
+        // Assertions below - do not modify them!
+        const changedContact = await collection.findOne({phoneNo: '48616161616'});
+        console.log(changedContact)
 
-    await client.close();
+        console.assert(changedContact !== null, 'Contact should be found by new phone number!', changedContact);
 
-    return process.exit(0);
-  } catch (err) {
-    console.log('Something went wrong!', err);
-    return process.exit(1);
-  }
+        const unchangedContact = await collection.findOne({phoneNo: '48754456654'});
+        console.assert(unchangedContact && unchangedContact.firstName === 'Jan' && unchangedContact.lastName === 'Nowacki',
+            'Should find unchanged contact with the same first name', unchangedContact);
+
+        await client.close();
+
+        return process.exit(0);
+    } catch (err) {
+        console.log('Something went wrong!', err);
+        return process.exit(1);
+    }
 })();
 
