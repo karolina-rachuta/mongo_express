e6d65c37f54168198abbd67476dc497e869a1fdb
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-03-19 19:34:39 +0100

    and

--- 06_Agregacja_danych/02_Operatory_zmiany_pol/movie-stats.js
+++ 06_Agregacja_danych/02_Operatory_zmiany_pol/movie-stats.js
@@ -1,5 +1,5 @@
 import MongoClient from 'mongodb';
-import { runAssertions } from './internals/assertions.js';
+import {runAssertions} from './internals/assertions.js';
 
 // Connection url
 const url = 'mongodb://localhost:27017';
@@ -11,30 +11,85 @@ const dbName = 'exercises';
 const collectionName = 'clothes';
 
 (async function () {
-  try {
-    // Connect using MongoClient
-    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
-    console.log('Successfully connected to local MongoDB instance.');
+    try {
+        // Connect using MongoClient
+        const client = await MongoClient.connect(url, {useUnifiedTopology: true});
+        console.log('Successfully connected to local MongoDB instance.');
 
-    // Get DB instance
-    const db = client.db(dbName);
+        // Get DB instance
+        const db = client.db(dbName);
 
-    const collection = db.collection(collectionName);
+        const collection = db.collection(collectionName);
 
-    let addUnsetData = [];
-    let projectData = [];
+        let addUnsetData = [];
+        let projectData = [];
 
-    // INSERT YOUR CODE HERE
+        // INSERT YOUR CODE HERE
 
-    // Assertions below
-    await runAssertions(addUnsetData, projectData);
+        //     addUnsetData = await collection.aggregate([
+        //         {
+        //             '$addFields': {
+        //                 'meanWatchTime': {'$avg': '$watchTime'},
+        //                 'meanRating': {'$avg': '$ratings'}
+        //             }
+        //         },
+        //         {
+        //             '$unset':
+        //                 [
+        //                     'watchTime', 'ratings'
+        //                 ]
+        //         }
+        //
+        // ]).toArray();
+        //
+        //     projectData = await collection.aggregate([
+        //         {
+        //             $project: {
+        //                 meanWatchTime: {$avg: '$watchTime'},
+        //                 meanRating: {'$avg': '$ratings'}
+        //             }
+        //         }
+        //     ])
+        addUnsetData = await collection.aggregate([
+            {
+                '$addFields': {
+                    'meanWatchTime': {
+                        '$avg': '$watchTime'
+                    },
+                    'meanRating': {
+                        '$avg': '$ratings'
+                    }
+                }
+            }, {
+                '$unset': [
+                    'watchTime', 'ratings'
+                ]
+            }
+        ]).toArray();
 
-    await client.close();
+        projectData = await collection.aggregate([
+            {
+                $project: {
+                    meanWatchTime: {$avg: '$watchTime'},
+                    meanRating: {$avg: '$ratings'}
+                }
+            }
+        ]).toArray();
 
-    return process.exit(0);
-  } catch (err) {
-    console.log('Something went wrong!', err);
-    return process.exit(1);
-  }
-})();
+        console.log(addUnsetData)
+        console.log(projectData)
+
+        // Assertions below
+        await runAssertions(addUnsetData, projectData);
+
+        await client.close();
+
+        return process.exit(0);
+    } catch
+        (err) {
+        console.log('Something went wrong!', err);
+        return process.exit(1);
+    }
+})
+();
 
